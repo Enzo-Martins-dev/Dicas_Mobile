e09c19c2f1402847d9bc20738d900fdc9b7d18a5
Enzo Martins <enzomartinsdev5@gmail.com>
2024-10-31 14:02:04 -0300

    Add Calendar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,6 +4,7 @@ import { View, Text, Button, StyleSheet, Alert } from "react-native";
 import { FavoritosContext } from '../context/ContextFavoritos';
 import { Calendar }  from 'react-native-calendars';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
 
 const Lista_dicas = require('../data/dados.json') 
 /**Obtendo os dados em dados.json, estão sendo armazenados em Lista_dicas */
@@ -21,16 +22,47 @@ let lista_dicas_favoritadas = []
 
 function HomeScreen( {navigation} ) {
     const [dica, setDica] = useState('')
-    const [data, setData] = useState(new Date())
     const { favoritos, setFavoritos } = useContext(FavoritosContext);
+    const [selectedDate, setSelectedDate] = useState('');
 
-    useEffect( () => {
+    const handleDayPress = (day) => {
+      setSelectedDate(day.dateString);
+    };
+
+    /*useEffect( () => {
       setDica(getDica(Lista_dicas))
-    }, [])
+    }, [])*/
+    useEffect( () => {
+      const carregarDica = async () => {
+        try {
+          const salvarDica = await AsyncStorage.getItem(selectedDate.toString())  
+          if (salvarDica != null) {
+            const elemento_presente = BuscaLinear(salvarDica, lista_dicas_favoritadas)[0] 
+            elemento_presente ? Alert.alert('Elemento já está na lista') : setDica(JSON.parse(salvarDica))
+          } else {
+            const novaDica = getDica(Lista_dicas)
+            setDica(novaDica)
+            await AsyncStorage.setItem(selectedDate.toString(), JSON.stringify(novaDica))
+          }
+        } catch (error) {
+          Alert.alert('Erro: ', error.message)
+        }
+      }
+      carregarDica()
+    }, [selectedDate])
+    /**Toda vez que tiver uma mudança na data selecionada no calendário (selectedDate), a função carregarDica() vai ser executada.
+     * Essa função vai salvar uma dica junto à data selecionada (SalvarDataDica + setDica(Json.parse)), relacionando esses dois valores.
+     * Se não tiver dica, vai ser selecionada uma dica da lista original e ser atribuida ao dia.
+     */
 
     return (
       <View style={styles.container}>
-        <Text> Bem-Vindo! A Dica do dia é: </Text>
+        <Calendar
+          onDayPress={handleDayPress}
+          markedDates={{ [selectedDate]: { selected: true } }}
+        />  
+
+        <Text> Bem-Vindo! A Dica do dia {selectedDate} é: </Text>
         <Text> {dica.text} </Text>
         <StatusBar style="auto" />
 
@@ -47,30 +79,37 @@ function HomeScreen( {navigation} ) {
 
     function FavoritarDica(dica) { 
       const [elemento_presente, i] = BuscaLinear(dica, lista_dicas_favoritadas);
-    
-      if (elemento_presente) {
-        Alert.alert('Dica já está nos favoritos')
-    
-      } else if (!elemento_presente) {
-        lista_dicas_favoritadas.push(dica)
-        setFavoritos([...lista_dicas_favoritadas]) /**Atualiza o valor de favoritos de acordo com a inserção de elementos na lista, usando uma
-        cópia dela. A lógica da remoção na função abaixo é a mesma. Eu dividi em duas funções pra manter o princípio de uma função fazer apenas
-        uma coisa.*/
-        Alert.alert("Dica adicionada aos favoritos")
+      try {
+        if (elemento_presente) {
+          Alert.alert('Dica já está nos favoritos')
+      
+        } else if (!elemento_presente) {
+          lista_dicas_favoritadas.push(dica)
+          setFavoritos([...lista_dicas_favoritadas]) /**Atualiza o valor de favoritos de acordo com a inserção de elementos na lista, usando uma
+          cópia dela. A lógica da remoção na função abaixo é a mesma. Eu dividi em duas funções pra manter o princípio de uma função fazer apenas
+          uma coisa.*/
+          Alert.alert("Dica adicionada aos favoritos")
+        }
+      } catch(erro) {
+        Alert.alert('Erro: ', erro.message)
       }
     }
     
     function DesfavoritarDica(dica) { 
       const [elemento_presente, i] = BuscaLinear(dica, lista_dicas_favoritadas);
-    
-      if (elemento_presente) {
-        lista_dicas_favoritadas.splice(i, 1)
-        setFavoritos([...lista_dicas_favoritadas]) 
-        Alert.alert('Dica Removida dos Favoritos')
-    
-      } else if (!elemento_presente) {
-        Alert.alert("Dica não está nos Favoritos")
+      try {
+        if (elemento_presente) {
+          lista_dicas_favoritadas.splice(i, 1)
+          setFavoritos([...lista_dicas_favoritadas]) 
+          Alert.alert('Dica Removida dos Favoritos')
+      
+        } else if (!elemento_presente) {
+          Alert.alert("Dica não está nos Favoritos")
+        }
+      } catch(erro) {
+        Alert.alert('Erro: ', erro.message)
       }
+      
     }
   }
 
